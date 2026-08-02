a12e855ac37b6218a2a8b76706671e1a03f298d0
Devyanshu Rathore <devyanshurathore219@gmail.com>
2026-08-02 23:57:31 +0530

    Add files via upload

---
+++ backend/config/token.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+
+const genToken = async (userId) => {
+    try {
+
+        const token = await jwt.sign(
+            { userId },
+            process.env.JWT_SECRET,
+            { expiresIn: "10d" }
+        )
+
+        return token
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default genToken
