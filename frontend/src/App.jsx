8a2835f765963fbfe6ce10d29f6af2cc9b71fa38
devyanshurathore219-cmd <devyanshurathore219@gmail.com>
2026-08-03 12:59:15 +0530

    chore(deps): add axios, react-icons, and react-router-dom
    Add axios for HTTP requests, react-icons for UI icons, and react-router-dom for client-side routing.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
 function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/signin' element={<SignIn />} />
+    </Routes>
   )
 }
 
